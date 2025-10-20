97930b41869d9495ac11f07924d934a7ce7fea3f
Kunal Krishna <kunalkrishna85@gmail.com>
2025-10-20 19:06:52 -0400

    feat: Add initial Spring Boot application

---
+++ backend/src/main/java/edu/unc/cs/heelp_ai/HeelpAiApplication.java
@@ -0,0 +1,13 @@
+package edu.unc.cs.heelp_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeelpAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeelpAiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/edu/unc/cs/heelp_ai/HeelpAiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unc.cs.heelp_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeelpAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
